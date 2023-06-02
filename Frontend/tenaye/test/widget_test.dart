94a263e6b3ef91ac7bf03a8b346510df2bfeb37d
GelilaT <gelilatefera575@gmail.com>
2023-06-02 23:52:51 +0300

    updated widget_test

--- Frontend/tenaye/test/widget_test.dart
+++ Frontend/tenaye/test/widget_test.dart
@@ -1,30 +1,157 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
 
+import 'package:tenaye/application/doctor/doctor_event.dart';
+import 'package:tenaye/application/user/user_state.dart';
+import 'package:tenaye/domain/doctor/doctor.dart';
 import 'package:tenaye/main.dart';
+import 'package:tenaye/presentation/doctor_text_edit.dart';
+import 'package:tenaye/presentation/appointmentListScreen.dart';
+import 'package:tenaye/presentation/bookAppointment.dart';
+import 'package:tenaye/presentation/doctorAppointment_list.dart';
+import 'package:tenaye/presentation/doctorDashboard.dart';
+import 'package:tenaye/domain/user/user.dart';
+import 'package:tenaye/application/user/user_bloc.dart';
+import 'package:tenaye/presentation/login.dart';
+import 'package:tenaye/presentation/profile.dart';
 
+@GenerateMocks([ProfileBloc])
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  
+testWidgets('Edit Doctor Profile form test', (WidgetTester tester) async {
+  await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  final usernameField = find.byKey(Key('add username'));
+  final emailField = find.byKey(Key('add email'));
+  final specializationField = find.byKey(Key('add specialization'));
+  final phoneNumberField = find.byKey(Key('add phone number'));
+  final feeField = find.byKey(Key('add fee'));
+  final cityField = find.byKey(Key('add city'));
+  final countryField = find.byKey(Key('add country'));
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  await tester.enterText(usernameField, 'John');
+  await tester.enterText(emailField, 'john@example.com');
+  await tester.enterText(specializationField, 'Cardiology');
+  await tester.enterText(phoneNumberField, '1234567890');
+  await tester.enterText(feeField, '100');
+  await tester.enterText(cityField, 'New York');
+  await tester.enterText(countryField, 'USA');
+  
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  await tester.tap(find.text('Save'));
+
+  await tester.pump();
+
+  expect((usernameField.evaluate().single.widget as TextFormField).controller?.text, '');
+  expect((emailField.evaluate().single.widget as TextFormField).controller?.text, '');
+  expect((specializationField.evaluate().single.widget as TextFormField).controller?.text, '');
+  expect((phoneNumberField.evaluate().single.widget as TextFormField).controller?.text, '');
+  expect((feeField.evaluate().single.widget as TextFormField).controller?.text, '');
+  expect((cityField.evaluate().single.widget as TextFormField).controller?.text, '');
+  expect((countryField.evaluate().single.widget as TextFormField).controller?.text, '');
+});
+
+testWidgets('Profile screen test', (WidgetTester tester) async {
+  // Create a mock user
+  final user = User(
+    id: "1",
+    email: 'abebe@example.com',
+    location: 'Addis Ababa',
+    username: 'abebe',
+    image: 'abebe.jpg',
+    password: "abebe123"
+
+  );
+
+  final profileBloc = MockProfileBloc();
+  when(profileBloc.state).thenReturn(ProfileLoadedState(user));
+
+  await tester.pumpWidget(
+    BlocProvider<ProfileBloc>.value(
+      value: profileBloc,
+      child: MaterialApp(
+        home: ProfilePage(),
+      ),
+    ),
+  );
+
+  expect(find.text('Email: ${user.email}'), findsOneWidget);
+  expect(find.text('Location: ${user.location}'), findsOneWidget);
+  expect(find.text('Username: ${user.username}'), findsOneWidget);
+
+  await tester.tap(find.widgetWithText(ElevatedButton, 'Edit Profile'));
+  await tester.pumpAndSettle();
+
+  expect(find.byType(UpdateProfilePage), findsOneWidget);
+
+  await tester.tap(find.widgetWithText(ElevatedButton, 'Delete Account'));
+  await tester.pumpAndSettle();
+
+  expect(find.byType(AlertDialog), findsOneWidget);
+
+  await tester.tap(find.widgetWithText(TextButton, 'Cancel'));
+  await tester.pumpAndSettle();
+
+  expect(find.byType(AlertDialog), findsNothing);
+
+  await tester.tap(find.widgetWithText(TextButton, 'Delete'));
+  await tester.pumpAndSettle();
+
+  verify(profileBloc.add()).called(1);
+
+  expect(find.byType(AlertDialog), findsNothing);
+
+  expect(find.byIcon(Icons.logout), findsOneWidget);
+
+  await tester.tap(find.byIcon(Icons.logout));
+  await tester.pumpAndSettle();
+
+  expect(find.byType(LoginPage), findsOneWidget);
+});
+
+late MockProfileBloc mockProfileBloc;
+  setUp(() {
+    mockProfileBloc = MockProfileBloc();
+  });
+  group('ProfilePage', () {
+    testWidgets('displays user profile and allows editing', (WidgetTester tester) async {
+      final user = User(
+        id: '1',
+        email: 'john@example.com',
+        location: 'New York',
+        username: 'John',
+        image: 'john.jpg',
+        password: 'john123'
+      );
+
+      final profileBloc = MockProfileBloc();
+      when(profileBloc.state).thenReturn(ProfileLoadedState(user));
+
+      await tester.pumpWidget(
+        BlocProvider<ProfileBloc>.value(
+          value: profileBloc,
+          child: MaterialApp(
+            home: ProfilePage(),
+          ),
+        ),
+      );
+
+      expect(find.text('Email: ${user.email}'), findsOneWidget);
+      expect(find.text('Location: ${user.location}'), findsOneWidget);
+      expect(find.text('Username: ${user.username}'), findsOneWidget);
+
+      await tester.tap(find.widgetWithText(ElevatedButton, 'Edit Profile'));
+      await tester.pumpAndSettle();
+
+      expect(find.byType(UpdateProfilePage), findsOneWidget);
+
+      await tester.tap(find.widgetWithText(ElevatedButton, 'Save'));
+      await tester.pumpAndSettle();
+
+      verify(profileBloc.add()).called(1);
+    });
   });
 }
