ce04120554e776888d14f6693f64d3a046f3eaaa
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2023-06-02 23:35:59 +0300

    Update patientRoute.js

--- backend/routes/patientRoute.js
+++ backend/routes/patientRoute.js
@@ -13,6 +13,7 @@ const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "public/images");
   },
+  
   filename: function (req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname));
   },
