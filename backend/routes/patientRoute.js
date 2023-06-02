211781c3c192ee86daca1417681baab2778dd36b
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2023-06-02 23:42:01 +0300

    Add API

--- backend/routes/patientRoute.js
+++ backend/routes/patientRoute.js
@@ -13,7 +13,6 @@ const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "public/images");
   },
-  
   filename: function (req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname));
   },
