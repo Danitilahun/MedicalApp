8a452c09e93d6ab700c84e98f3d345e380bfe19c
yohannes-07 <106800042+yohannes-07@users.noreply.github.com>
2023-06-02 23:34:35 +0300

    Add API

--- backend/models/userTokenModel.js
+++ backend/models/userTokenModel.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
-
 const userTokenSchema = new Schema({
     _id: { type: Schema.Types.ObjectId, required: true },
     token: { type: String, required: true },
